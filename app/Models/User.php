d5082a88dddbd2e0df49c06e5c6e9f3461b8bb12
Adrian <s39887@s.pwste.edu.pl>
2024-05-28 20:59:59 +0200

    yes

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username',
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+        'address',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
-        'password', 'remember_token',];
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function wypozyczenia()
-    {
-        return $this->hasMany(Wypozyczenia::class);
-    }
-
-
-
 }
-Schema::table('users', function (Blueprint $table) {
-    $table->string('role')->default('user');
-});
