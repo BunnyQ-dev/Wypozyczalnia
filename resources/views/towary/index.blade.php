ac95bc2a53b6ec4fcc29247aa8331f807d4f1fd0
Adrian <s39887@s.pwste.edu.pl>
2024-05-28 21:48:55 +0200

    yes

--- resources/views/towary/index.blade.php
+++ resources/views/towary/index.blade.php
@@ -1,30 +1,27 @@
+<!-- resources/views/towary/index.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
     <h1>Lista Towarów</h1>
-    <a href="{{ route('towary.create') }}" class="btn btn-primary mb-3">Dodaj nowy towar</a>
     <table class="table">
         <thead>
         <tr>
             <th>Nazwa</th>
+            <th>Opis</th>
             <th>Cena</th>
             <th>Kategoria</th>
-            <th>Akcje</th>
+            <th>Dostępność</th>
         </tr>
         </thead>
         <tbody>
         @foreach($towary as $towar)
             <tr>
                 <td>{{ $towar->nazwa }}</td>
+                <td>{{ $towar->opis }}</td>
                 <td>{{ $towar->cena }}</td>
                 <td>{{ $towar->kategoria->nazwa }}</td>
-                <td>
-                    <form action="{{ route('towary.destroy', $towar->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Usuń</button>
-                    </form>
-                </td>
+                <td>{{ $towar->dostepnosc ? 'Dostępna' : 'Niedostępna' }}</td>
             </tr>
         @endforeach
         </tbody>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TowarController;
 use App\Http\Controllers\WypozyczeniaController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\MaszynyController;
 
 // Trasa główna przekierowująca na stronę logowania
 Route::get('/', function () {
@@ -20,22 +19,19 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Trasa dla strony głównej po zalogowaniu
 Route::get('/home', function () {
-    return view('welcome'); // Możesz zmienić na inny istniejący widok
+    return redirect()->route('towary.index');
 })->name('home')->middleware('auth');
 
 // Trasy dla TowarController
-Route::get('/towary', [TowarController::class, 'index'])->name('towary.index');
-Route::get('/towary/create', [TowarController::class, 'create'])->name('towary.create');
-Route::post('/towary', [TowarController::class, 'store'])->name('towary.store');
-Route::delete('/towary/{id}', [TowarController::class, 'destroy'])->name('towary.destroy');
+Route::get('/towary', [TowarController::class, 'index'])->name('towary.index')->middleware('auth');
+Route::get('/towary/create', [TowarController::class, 'create'])->name('towary.create')->middleware('auth');
+Route::post('/towary', [TowarController::class, 'store'])->name('towary.store')->middleware('auth');
+Route::delete('/towary/{id}', [TowarController::class, 'destroy'])->name('towary.destroy')->middleware('auth');
 
 // Trasy dla WypozyczeniaController
-Route::get('/wypozyczenia', [WypozyczeniaController::class, 'index'])->name('wypozyczenia.index');
-Route::get('/wypozyczenia/create', [WypozyczeniaController::class, 'create'])->name('wypozyczenia.create');
-Route::post('/wypozyczenia', [WypozyczeniaController::class, 'store'])->name('wypozyczenia.store');
-Route::get('/wypozyczenia/{id}/edit', [WypozyczeniaController::class, 'edit'])->name('wypozyczenia.edit');
-Route::put('/wypozyczenia/{id}', [WypozyczeniaController::class, 'update'])->name('wypozyczenia.update');
-Route::delete('/wypozyczenia/{id}', [WypozyczeniaController::class, 'delete'])->name('wypozyczenia.delete');
-
-// Trasy dla MaszynyController
-Route::get('/maszyny', [MaszynyController::class, 'index'])->name('maszyny.index')->middleware('auth');
+Route::get('/wypozyczenia', [WypozyczeniaController::class, 'index'])->name('wypozyczenia.index')->middleware('auth');
+Route::get('/wypozyczenia/create', [WypozyczeniaController::class, 'create'])->name('wypozyczenia.create')->middleware('auth');
+Route::post('/wypozyczenia', [WypozyczeniaController::class, 'store'])->name('wypozyczenia.store')->middleware('auth');
+Route::get('/wypozyczenia/{id}/edit', [WypozyczeniaController::class, 'edit'])->name('wypozyczenia.edit')->middleware('auth');
+Route::put('/wypozyczenia/{id}', [WypozyczeniaController::class, 'update'])->name('wypozyczenia.update')->middleware('auth');
+Route::delete('/wypozyczenia/{id}', [WypozyczeniaController::class, 'delete'])->name('wypozyczenia.delete')->middleware('auth');
