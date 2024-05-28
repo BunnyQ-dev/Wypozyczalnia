815a5f601857554758c1a5a94810129c2775f844
Adrian <s39887@s.pwste.edu.pl>
2024-05-28 17:09:44 +0200

    yes

--- database/migrations/2024_05_28_105635_create_users_table.php
+++ database/migrations/2024_05_28_105635_create_users_table.php
@@ -4,17 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Creates an unsignedBigInteger primary key column
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
@@ -28,11 +26,9 @@ class CreateUsersTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
