7ec93fd2dc2a75a25210a9db36d855c77992c525
s39887 <s39887@s.pwste.edu.pl>
2024-05-29 11:33:30 +0200

    yes

--- database/migrations/2024_05_28_082457_create_towars_table.php
+++ database/migrations/2024_05_28_082457_create_towars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTowarTable extends Migration
+class CreateTowarsTable extends Migration
 {
     /**
      * Run the migrations.
