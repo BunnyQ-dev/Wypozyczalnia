0c1c0ca8a49e78a0156e0a80c0241afa90766237
s39907 <s39907@s.pwste.edu.pl>
2024-05-29 10:11:38 +0200

    wypozyczenia

--- resources/views/towary/create.blade.php
+++ resources/views/towary/create.blade.php
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<form action="{{ route('towary.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="nazwa">Nazwa:</label>
-        <input type="text" name="nazwa" id="nazwa" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="opis">Opis:</label>
-        <textarea name="opis" id="opis" class="form-control"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="cena">Cena:</label>
-        <input type="number" name="cena" id="cena" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="kategoria_id">Kategoria:</label>
-        <select name="kategoria_id" id="kategoria_id" class="form-control" required>
-            <option value="">Wybierz kategorię</option>
-            @foreach($kategorie as $kategoria)
-                <option value="{{ $kategoria->id }}">{{ $kategoria->nazwa }}</option>
-            @endforeach
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary">Dodaj</button>
-</form>
-=======
 @extends('layouts.app')
 
 @section('title', 'Dodaj Nowy Towar')
@@ -54,4 +27,3 @@
         </form>
     </div>
 @endsection
->>>>>>> origin/wypozyczenie
